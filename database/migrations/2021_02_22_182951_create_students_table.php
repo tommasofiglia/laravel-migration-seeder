7d619e83e83e133cf04d71d52e7d891439e314ba
Tommaso Figlia <tommasofiglia@gmail.com>
2021-02-22 20:19:48 +0100

    Creato il seeder Student con realativa migration e model

---
+++ database/migrations/2021_02_22_182951_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('lastname', 30);
+            $table->int('age');
+            $table->string('email', 50);
+            $table->string('degree', 20);
+            $table->string('classname', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ database/seeds/StudentSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use app\Student;
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $students = [
+          [
+            "name" => '',
+            "lastname" => '',
+
+
+          ]
+        ]
+    }
+}
